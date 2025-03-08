fc9a729b9f5f7aba781f143cfc5e8f75dab2870f
Gabriel Ramirez Vilchis <gramirez@lcg.unam.mx>
2025-03-07 21:16:24 -0600

    Conda environment activation removed

--- src/sequences_download.sh
+++ src/sequences_download.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# Activar el ambiente donde esta SRA toolkit
-conda activate sra-toolkit
-
-# Descargar el archivo .sra
-prefetch SRR8615998 && fastq-dump --split-files --skip-technical SRR1234567.sra
-
+# Downloading sequences from NCBI SRA database
+nohup prefetch SRR8615998 && fastq-dump --split-files SRR1234567.sra &
