3fc8f8ceadcc74a3352ae638fa042d3d3db5c4cd
carlosgglez <carlosgg@lcg.unam.mx>
2025-03-11 16:03:06 -0600

    Se actualizo el script para hacer la descarga de una sola secuencia

--- src/sequences_download.sh
+++ src/sequences_download.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-# Downloading sequences from NCBI SRA database
-nohup prefetch SRR8615998 && fastq-dump --split-files SRR1234567.sra &
+# Descargar las secuencias de RNA-Seq del repositorio SRA en segundo plano
+nohup prefetch SRR8615998 &
+
+# Convertir el archivo .sra a formato FASTQ, separando los pares de lecturas y omitiendo datos técnicos
+fastq-dump --split-files --skip-technical SRR8615998/SRR8615998.sra
+
+# Evaluar la calidad de las lecturas con FastQC
+fastqc SRR8615998_1.fastq
+fastqc SRR8615998_2.fastq
+
+# Revisar los reportes HTML generados para verificar la calidad de las secuencias
+# Como la calidad de las secuencias es buena, no es necesario aplicar trimming con trim_galore o trimmomatic
+
+# Construir un índice de transcritos para Kallisto a partir de la referencia de Homo sapiens
+kallisto index -i Homo_sapiens.GRCh38.cdna.all.release-94_k31.idx Homo_sapiens.GRCh38.cdna.all.fa
+
+# Cuantificar la expresión génica usando Kallisto con las lecturas obtenidas
+kallisto quant -i Homo_sapiens.GRCh38.cdna.all.release-94_k31.idx -o alineamiento_SRR8615998 SRR8615998_1.fastq SRR8615998_2.fastq
